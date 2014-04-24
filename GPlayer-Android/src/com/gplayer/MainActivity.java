6bc4ec8c978b7a6687a710f9a44314eef4339b5a
Krishna Goutham <gouthambachu@gmail.com>
2014-04-24 12:43:35 +0530

    GCM registering refactored

--- GPlayer-Android/src/com/gplayer/MainActivity.java
+++ GPlayer-Android/src/com/gplayer/MainActivity.java
@@ -270,7 +270,7 @@ public class MainActivity extends Activity implements Observer
         
         if (isConnected)
         {
-            //registerGCM();
+            registerGCM();
         }
     }
     
